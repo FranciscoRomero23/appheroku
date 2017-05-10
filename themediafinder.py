ee327454988ba7e2be662d5774956bd6ff45beb3
Francisco Romero <fjo7o6@hotmail.com>
2017-05-10 14:34:44 +0200

    Estoy haciendo pruebas.

--- themediafinder.py
+++ themediafinder.py
@@ -10,8 +10,7 @@ def inicio():
     
 @route('/cartelera') 
 def cartelera():
-	payload={"query":busqueda,"api_key":os.environ["key_tmdb"],"language":"es-ES"}
-	r = requests.get("https://api.themoviedb.org/3/movie/now_playing",params=payload)
+	r = requests.get("https://api.themoviedb.org/3/movie/now_playing?api_key=35bcc7d68551a6d39bc6bef1847e87b5&language=es-ES")
 	js=json.loads(r.text)
 	return template("html/cartelera.tpl",js=js)
 
