4a90668f23582ea149fc379845a2c92dd7eca005
Francisco Romero <fjo7o6@hotmail.com>
2017-05-23 13:33:28 +0200

    Probando twitter

--- themediafinder.py
+++ themediafinder.py
@@ -209,7 +209,7 @@ def twittear(valorado):
                        resource_owner_key=TOKENS["access_token"],
                        resource_owner_secret=TOKENS["access_token_secret"])
       url = 'https://api.twitter.com/1.1/statuses/update.json'
-      status = 'Me ha gustado la pelicula %s'%valorado
+      status = 'Me ha gustado %s. via themediafinder.herokuapp.com'%valorado
       r = requests.post(url=url,data={"status":status},auth=oauth)
       if r.status_code == 200:
         return """<h2>Tweet Enviado Corrrectamente</h2>"""
